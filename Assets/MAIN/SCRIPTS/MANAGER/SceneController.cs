bac9e96bc9e1e5d6306017c113ac4628535e6d98
Dmitriy <4aynoedepebo@gmail.com>
2021-02-22 16:34:03 +0200

    * missing assets

---
+++ Assets/MAIN/SCRIPTS/MANAGER/SceneController.cs
@@ -0,0 +1,128 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class SceneController : MonoBehaviour {
+
+    private static Dictionary<string, bool> sceneStateTracker;
+    public static SceneController sceneController;
+    bool gameStart;
+    public static string initScene = "";
+
+    //public AdmobController admob;
+
+    public void Awake()
+    {
+        Application.targetFrameRate = 60;
+        Debug.Log(" >>>>> scene " + initScene);
+        if (!gameStart)
+        {
+            if (sceneStateTracker == null)
+            {
+                sceneStateTracker = new Dictionary<string, bool> {
+                    { "main", false },
+                    { "result", false },
+                    { "shop", false }
+                };
+            }
+            sceneController = this;
+            if (initScene != null && "main".Equals(initScene))
+            {
+                sceneStateTracker["main"] = false;
+                LoadGame();
+            }
+            else if (initScene != null && "result".Equals(initScene))
+            {
+                sceneStateTracker["result"] = false;
+                LoadResult();
+            }
+            else if (initScene != null && "shop".Equals(initScene))
+            {
+                sceneStateTracker["shop"] = false;
+                LoadShop();
+            }
+            else
+            {
+                LoadGame();
+            }
+            gameStart = true;
+        }
+
+        Debug.Log("I'm loadin services here !");
+    }
+
+    private void UnloadScene(string scene)
+    {
+        //Debug.Log("> u >" + sceneStateTracker[scene]);
+        if (sceneStateTracker[scene])
+        {
+            StartCoroutine(Unload(scene));
+            sceneStateTracker[scene] = false;
+        }
+    }
+
+    private void UnloadScene(int scene)
+    {
+        StartCoroutine(Unload(scene));
+    }
+
+    private void LoadScene(string scene)
+    {
+        if (!sceneStateTracker[scene])
+        {
+            //Debug.Log("> l >" + scene);
+            SceneManager.LoadSceneAsync(scene, LoadSceneMode.Additive);
+            sceneStateTracker[scene] = true;
+        }
+    }
+
+    public void LoadGame()
+    {
+        UnloadScene("result");
+        UnloadScene("shop");
+        LoadScene("main");
+    }
+
+    public void LoadResult()
+    {
+        UnloadScene("main");
+        UnloadScene("shop");
+        LoadScene("result");
+    }
+
+    public void LoadShop()
+    {
+        UnloadScene("main");
+        UnloadScene("result");
+        LoadScene("shop");
+    }
+
+    //public void UnloadGame()
+    //{
+    //    UnloadScene("main");
+    //}
+
+    //public void UnloadMenu()
+    //{
+    //    UnloadScene("result");
+    //}
+
+    private IEnumerator Unload(string scene)
+    {
+        yield return null;
+        SceneManager.UnloadSceneAsync(scene);
+    }
+
+    private IEnumerator Unload(int scene)
+    {
+        yield return null;
+        SceneManager.UnloadSceneAsync(scene);
+    }
+
+    public void RestartGame()
+    {
+        initScene = "main";
+        LoadGame();
+    }
+}
---
+++ Assets/MAIN/SCRIPTS/UTIL/DataController.cs
@@ -0,0 +1,248 @@
+﻿using UnityEngine;
+using System.Collections.Generic;
+using System.IO;
+using System;
+
+public class DataController {
+    private static string shopItemsFileName = "shopItems.json";
+    //private static string playerFileName = "playerInfo.json";
+
+    public static string AjsonData;
+    private const string CASHE_FOLDER = "LocalStorage";
+
+#if UNITY_EDITOR
+    public static string shopItemsFilePath = Path.Combine(Application.dataPath + "/StreamingAssets", shopItemsFileName);
+    //public static string playerfilePath = Path.Combine(Application.dataPath + "/StreamingAssets", playerFileName);
+    //public static string playerfilePath = Path.Combine(Application.persistentDataPath, playerFileName);
+
+#elif UNITY_ANDROID
+    public static string shopItemsFilePath = Path.Combine ("jar:file://" + Application.dataPath + "!/assets/", shopItemsFileName);
+
+#elif UNITY_IOS
+    private static string shopItemsFilePath = Path.Combine (Application.persistentDataPath  + "/Raw", shopItemsFileName);
+#endif
+
+    ////--------- Player Info ----------
+    //public static PlayerData LoadPlayer()
+    //{
+    //    string jsonData = "";
+    //    //if (Application.platform == RuntimePlatform.Android) {
+
+    //    if (!File.Exists(playerfilePath))
+    //    {
+    //        TextAsset file = Resources.Load("playerInfo") as TextAsset;
+    //        jsonData = file.ToString();
+    //        PlayerData pi = JsonUtility.FromJson<PlayerData>(jsonData);
+    //        pi.firstLoginAt = DateTime.Now.ToString("yyyy-MM-dd HH:mm:ss");
+    //        return pi;
+    //        //StreamReader f = new StreamReader(playerfilePath);
+    //        //jsonData = f.ReadToEnd();
+    //        //f.Close();
+
+
+    //        //WWW reader = new WWW(playerfilePath);
+    //        //while (!reader.isDone) { }
+    //        //jsonData = reader.text;
+    //    }
+    //    else
+    //    {
+    //        jsonData = File.ReadAllText(playerfilePath);
+    //        PlayerData pi = JsonUtility.FromJson<PlayerData>(jsonData);
+    //        Debug.Log(">>>> jsonData > " + jsonData);
+    //        return pi;
+
+    //    }
+    //    // PlayerData pi = JsonUtility.FromJson<PlayerData>(jsonData);
+    //    //   AjsonData = "<color=#a52a2aff> " + jsonData + "</color>";
+
+    //}
+
+    //public static void SavePlayer(PlayerData pi)
+    //{
+    //    string jsonData = JsonUtility.ToJson(pi);
+    //    // Debug.Log(">>> player info > " + jsonData);
+    //    // if (Application.platform == RuntimePlatform.Android) {
+    //    StreamWriter writer = new StreamWriter(playerfilePath, false);
+    //    writer.WriteLine(jsonData);
+    //    writer.Close();
+    //    //} else {
+    //    //    File.WriteAllText(playerfilePath, jsonData);
+    //    //} 
+    //}
+
+    //------------Items ---------------------------
+
+    // items
+    public static List<ItemShopData> LoadShopItems()
+    {
+        string jsonData = "";
+        if (Application.platform == RuntimePlatform.Android)
+        {
+            WWW reader = new WWW(shopItemsFilePath);
+            while (!reader.isDone) { }
+            jsonData = reader.text;
+        }
+        else
+        {
+            jsonData = File.ReadAllText(shopItemsFilePath);
+        }
+        ItemShopData[] id = JsonHelper.FromJson<ItemShopData>(jsonData);
+        return new List<ItemShopData>(id);
+    }
+
+//    //------------Load Levels --------------------
+//    public static List<LevelData> LoadLevels()
+//    {
+//        List<LevelData> lvlsData = new List<LevelData>();
+//        string jsonData = "";
+//        if (Application.platform == RuntimePlatform.Android)
+//        {
+//            WWW reader = new WWW(levelfilePath);
+//            while (!reader.isDone) { }
+
+//            jsonData = reader.text;
+//        }
+//        else
+//        {
+//            jsonData = File.ReadAllText(levelfilePath);
+//        }
+
+
+//        if (jsonData != null)
+//        {
+//            string separ = "Level_[0-9][0-9][0-9]";
+//            string[] lvls = System.Text.RegularExpressions.Regex.Split(jsonData, separ);
+
+//            for (int i = 1; i < lvls.Length; i++)
+//            {
+
+//                string editedLvl = "{ \"Rows" + lvls[i].Substring(1, lvls[i].Length - 7);
+//                if (editedLvl.LastIndexOf("]") == editedLvl.Length - 1)
+//                {
+//                    editedLvl += "}";
+//                }
+//                else
+//                {
+//                    editedLvl += "]}";
+//                }
+//                RowData[] rows = JsonHelper.FromJson<RowData>(editedLvl);
+//                LevelData lvlData = new LevelData();
+//                System.Array.Reverse(rows);
+
+//                foreach (RowData row in rows)
+//                {
+//                    if (!row.IsEmpty())
+//                    {
+//                        lvlData.rows.Add(row);
+//                    }
+//                }
+
+//                foreach (RowData row in rows)
+//                {
+//                    if (row.IsEmpty())
+//                    {
+//                        lvlData.emptyRowsCount++;
+//                    }
+//                    else
+//                    {
+//                        break;
+//                    }
+//                }
+
+//                lvlsData.Add(lvlData);
+//            }
+//            return lvlsData;
+//        }
+//        else
+//        {
+//            //Debug.LogError("Cannot find the file " + levelfilePath);
+//            //AjsonData = "<color=#a52a2aff>JSON IS NULL</color>";
+//            return lvlsData;
+//        }
+//    }
+
+}
+
+//// PLAYER DATA
+//[System.Serializable]
+//public class PlayerData {
+//    public int gems;
+//    public int stars;
+//    public int progressTowardsNextStarBox; // how many stars player already gathered for the current star box
+//    public int numStarBoxesOpened; // how many boxs were already opened
+//    public string giveBoxAt;
+//    public string firstLoginAt;
+//    public bool noAds;
+//    public string specialBallImageName;
+//    public string specialBallName;
+//    public bool boughtStarter;
+
+//    public List<CompletedLevel> completedLvls = new List<CompletedLevel>();
+//    public List<ItemData> items = new List<ItemData>();
+
+//    public int GetStarsAmount()
+//    {
+//        int sum = 0;
+//        foreach (CompletedLevel lvl in completedLvls)
+//        {
+//            sum += lvl.stars;
+//        }
+//        stars = sum;
+//        return sum;
+//    }
+//}
+
+[System.Serializable]
+public class ItemShopData {
+    public bool isBought;
+    public string name;
+    public string description;
+    public int priceBJ;
+    //public string idSKU;
+    //public string priceSKU;
+    public string imageIcon;
+    public string imageAsset;
+    public string bodyPart;
+
+    public ItemShopData(ShopItemObject sio)
+    {
+        isBought = sio.isBought;
+        name = sio.name;
+        description = sio.description;
+        priceBJ = sio.priceBJ;
+        imageIcon = sio.imageIcon;
+        imageAsset = sio.imageAsset;
+        bodyPart = sio.bodyPart;
+    }
+    public ItemShopData()
+    {
+
+    }
+}
+
+public static class JsonHelper {
+    public static T[] FromJson<T>(string json)
+    {
+        Wrapper<T> wrapper = JsonUtility.FromJson<Wrapper<T>>(json);
+        return wrapper.Rows;
+    }
+
+    public static string ToJson<T>(T[] array)
+    {
+        Wrapper<T> wrapper = new Wrapper<T>();
+        wrapper.Rows = array;
+        return JsonUtility.ToJson(wrapper);
+    }
+
+    public static string ToJson<T>(T[] array, bool prettyPrint)
+    {
+        Wrapper<T> wrapper = new Wrapper<T>();
+        wrapper.Rows = array;
+        return JsonUtility.ToJson(wrapper, prettyPrint);
+    }
+
+    [System.Serializable]
+    private class Wrapper<T> {
+        public T[] Rows;
+    }
+}
